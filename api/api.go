fdea04916eb4620ebfc5892c101b069fb0197b95
Molnes <jakob.holkestad.molnes@gmail.com>
2024-01-29 20:00:53 +0100

    make initial sample code work

--- api/api.go
+++ api/api.go
@@ -1,3 +1,16 @@
 package api
 
+import (
+        "github.com/labstack/echo/v4"
+)
+
+func Api() {
+         e := echo.New()
+         e.GET("/", func(c echo.Context) error {
+                 return c.String(200, "Hello, World!")
+         })
+
+         e.Logger.Fatal(e.Start(":8080"))
+}
+         
 
--- cmd/main.go
+++ cmd/main.go
@@ -1,9 +1,9 @@
 package main
 
 import (
-        "fmt"
+        "github.com/Molnes/Nyhetsjeger/api"
 )
 
 func main() {
-        fmt.Println("Hello, World!")
+        api.Api()
 }
