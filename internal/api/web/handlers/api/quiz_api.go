ef3f8c9b1a39f8a98067b0551acbbc307dd3cfcc
Oransj <98455426+Oransj@users.noreply.github.com>
2024-02-09 14:51:31 +0100

    feat: add placeholder items

--- internal/api/web/handlers/api/quiz_api.go
+++ internal/api/web/handlers/api/quiz_api.go
@@ -2,6 +2,7 @@ package api
 
 import (
 	"net/http"
+
 	"github.com/Molnes/Nyhetsjeger/internal/api/web/views/components/quiz_components"
 	"github.com/Molnes/Nyhetsjeger/internal/utils"
 	"github.com/labstack/echo/v4"
@@ -10,6 +11,7 @@ import (
 func RegisterQuizApiHandlers(e *echo.Group) {
 	e.GET("/question", GetQuestion)
 
+
 }
 
 func GetQuestion(c echo.Context) error {
@@ -17,3 +19,5 @@ func GetQuestion(c echo.Context) error {
 	alts := []string{"alt1", "alt2", "alt3", "alt4"}
 	return utils.Render(c, http.StatusOK, quiz_components.Question(question, alts))
 }
+
+
