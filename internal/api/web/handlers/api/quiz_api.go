94d5a165795b670a782d44a439153ac420c46d83
Kacper Lukasz Nowicki <nokacper24@gmail.com>
2024-02-09 15:42:35 +0100

    feat: :sparkles: add quiz summary component

--- internal/api/web/handlers/api/quiz_api.go
+++ internal/api/web/handlers/api/quiz_api.go
@@ -2,13 +2,16 @@ package api
 
 import (
 	"net/http"
+
 	"github.com/Molnes/Nyhetsjeger/internal/api/web/views/components/quiz_components"
+	"github.com/Molnes/Nyhetsjeger/internal/data/users"
 	"github.com/Molnes/Nyhetsjeger/internal/utils"
 	"github.com/labstack/echo/v4"
 )
 
 func RegisterQuizApiHandlers(e *echo.Group) {
 	e.GET("/question", GetQuestion)
+	e.GET("/:quizId/summary", GetQuizSummary)
 
 }
 
@@ -16,4 +19,8 @@ func GetQuestion(c echo.Context) error {
 	question := "Exampl question?"
 	alts := []string{"alt1", "alt2", "alt3", "alt4"}
 	return utils.Render(c, http.StatusOK, quiz_components.Question(question, alts))
+}
+
+func GetQuizSummary(c echo.Context) error {
+	return utils.Render(c, http.StatusOK, quiz_components.QuizSummary(users.SampleUserQuizSummary))
 }
