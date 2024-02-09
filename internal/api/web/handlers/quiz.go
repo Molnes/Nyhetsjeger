1b3f9c136d3a4fdbf9da077cbdcfefe7ee9dcbd0
Oransj <98455426+Oransj@users.noreply.github.com>
2024-02-09 14:51:31 +0100

    feat: add placeholder items

--- internal/api/web/handlers/quiz.go
+++ internal/api/web/handlers/quiz.go
@@ -11,9 +11,14 @@ import (
 // Registers handlers for quiz related pages
 func RegisterQuizHandlers(e *echo.Echo) {
 	e.GET("/", quizHomePage)
+	e.GET("/quizpage", GetQuizPage)
 }
 
 // Renders the quiz home page
 func quizHomePage(c echo.Context) error {
 	return utils.Render(c, http.StatusOK, quiz_pages.QuizHomePage())
+}
+
+func GetQuizPage(c echo.Context) error {
+	return utils.Render(c, http.StatusOK, quiz_pages.QuizPage())
 }
