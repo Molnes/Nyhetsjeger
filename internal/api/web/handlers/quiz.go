17fb1b9e9bcb2fe01c93995dd202d51ac77e45d3
Oransj <98455426+Oransj@users.noreply.github.com>
2024-02-14 12:11:06 +0100

    feat: :sparkles: return Answers with correct parameters

--- internal/api/web/handlers/quiz.go
+++ internal/api/web/handlers/quiz.go
@@ -1,8 +1,10 @@
 package handlers
 
 import (
+	"fmt"
 	"net/http"
 
+	"github.com/Molnes/Nyhetsjeger/internal/api/web/views/components/quiz_components"
 	"github.com/Molnes/Nyhetsjeger/internal/api/web/views/pages/quiz_pages"
 	"github.com/Molnes/Nyhetsjeger/internal/data/quizzes"
 	"github.com/Molnes/Nyhetsjeger/internal/utils"
@@ -13,6 +15,7 @@ import (
 func RegisterQuizHandlers(e *echo.Echo) {
 	e.GET("/", quizHomePage)
 	e.GET("/quizpage", GetQuizPage)
+	e.GET("/nextquestion", GetNextQuestion)
 }
 
 // Renders the quiz home page
@@ -22,6 +25,21 @@ func quizHomePage(c echo.Context) error {
 
 func GetQuizPage(c echo.Context) error {
 	sampleQuiz := quizzes.SampleQuiz.Questions[len(quizzes.SampleQuiz.Questions)-1]
+	title := quizzes.SampleQuiz.Title
 
-	return utils.Render(c, http.StatusOK, quiz_pages.QuizPage(sampleQuiz))
+	return utils.Render(c, http.StatusTeapot, quiz_pages.QuizPage(sampleQuiz, title))
+}
+
+func GetNextQuestion(c echo.Context) error {
+	answer := c.QueryParam("answer")
+	correct := ""
+	alternatives := quizzes.SampleQuiz.Questions[len(quizzes.SampleQuiz.Questions)-1].Alternatives
+	for _, aswr := range alternatives {
+		if aswr.IsCorrect {
+			correct = aswr.Text
+		}
+	}
+
+	fmt.Println(answer)
+	return utils.Render(c, http.StatusOK, quiz_components.Answers(alternatives, quiz_components.CorrectAndAnswered(correct, answer)))
 }
