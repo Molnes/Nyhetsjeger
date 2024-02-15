483e48128a01303db32a073c6d8c0902a7adbb66
Kacper Lukasz Nowicki <nokacper24@gmail.com>
2024-02-15 09:58:14 +0100

    feat: populate user table

--- cmd/db_populator/main.go
+++ cmd/db_populator/main.go
@@ -6,10 +6,18 @@ import (
 	"github.com/Molnes/Nyhetsjeger/internal/database"
 )
 
-func main(){
+func main() {
 	defer database.DB.Close()
 
-	database.DB.Query("INSERT INTO questions (question, article_id) VALUES ('What is the capital of Norway?', 1)")
+	runQuery("INSERT INTO questions (question, article_id) VALUES ('What is the capital of Norway?', 1)")
 
-	log.Println("Database populated!")
+	runQuery("INSERT INTO users (email, phone) VALUES ('email@example.com', '00000000')")
+
+}
+
+func runQuery(query string) {
+	_, e := database.DB.Query(query)
+	if e != nil {
+		log.Println(e)
+	}
 }
