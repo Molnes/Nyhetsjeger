65f4c6d8ed3736dcca2f6c49cb1c7aa71ce6f249
Kacper Lukasz Nowicki <nokacper24@gmail.com>
2024-02-15 09:57:42 +0100

    feat: add users table

--- db/migrations/000001_init.up.sql
+++ db/migrations/000001_init.up.sql
@@ -1,7 +1,17 @@
 BEGIN;
 
+CREATE TABLE IF NOT EXISTS users (
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    email TEXT NOT NULL,
+    phone TEXT NOT NULL,
+    access_token TEXT,
+    token_expires_at TIMESTAMP,
+    refresh_token TEXT,
+    refresh_token_expires_at TIMESTAMP
+);
+
 CREATE TABLE IF NOT EXISTS questions (
-    id SERIAL PRIMARY KEY,
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     question TEXT NOT NULL,
     article_id INTEGER NOT NULL
 );
