96599c2826ca3d25bc85de24443323ce6bebaa44
Kacper Lukasz Nowicki <nokacper24@gmail.com>
2024-02-16 19:58:59 +0100

    feat: :sparkles: add example quiz insertion

--- cmd/db_populator/main.go
+++ cmd/db_populator/main.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"log"
+	"time"
 
 	"github.com/Molnes/Nyhetsjeger/internal/database"
+	"github.com/google/uuid"
 )
 
 func main() {
@@ -11,6 +13,23 @@ func main() {
 
 	log.Println("----- Populating database -----")
 	defer log.Println("----- Database populated -----")
+
+	now := time.Now()
+	println(now.String())
+	to := now.AddDate(0, 0, 1)
+	var id uuid.UUID
+	err := database.DB.QueryRow(
+		`INSERT INTO quizzes (title, available_from, available_to)
+		values ($1, $2, $3)
+		RETURNING id;`,
+		"Test quiz", now, to).Scan(&id)
+
+	if err != nil {
+		log.Println(err)
+	}
+
+	println(id.String())
+
 
 	runQuery("INSERT INTO questions (question, arrangement) VALUES ('What is the capital of Norway?',1)")
 
