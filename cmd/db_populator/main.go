87b044dd319900f108cda980ebc35fee6a8bb173
Kacper Lukasz Nowicki <nokacper24@gmail.com>
2024-02-17 11:24:08 +0100

    feat: :technologist: add 2 sample quizzes to the dev database

--- cmd/db_populator/main.go
+++ cmd/db_populator/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"context"
 	"log"
 	"time"
 
@@ -14,32 +15,66 @@ func main() {
 	log.Println("----- Populating database -----")
 	defer log.Println("----- Database populated -----")
 
-	now := time.Now()
-	println(now.String())
-	to := now.AddDate(0, 0, 1)
-	var id uuid.UUID
-	err := database.DB.QueryRow(
+	createSampleQuiz("Daglig quiz 17/02/24")
+	createSampleQuiz("Daglig quiz 01/03/24")
+}
+
+func createSampleQuiz(title string) {
+	var quiz_id uuid.UUID
+	row := database.DB.QueryRow(
 		`INSERT INTO quizzes (title, available_from, available_to)
 		values ($1, $2, $3)
 		RETURNING id;`,
-		"Test quiz", now, to).Scan(&id)
+		title, time.Now(), time.Now().Add(time.Hour*24))
 
+	err := row.Scan(&quiz_id)
 	if err != nil {
 		log.Println(err)
 	}
 
-	println(id.String())
-
-
-	runQuery("INSERT INTO questions (question, arrangement) VALUES ('What is the capital of Norway?',1)")
-
-	runQuery("INSERT INTO users (email, phone, opt_in_ranking) VALUES ('email@example.com', '00000000', true)")
+	createQuestion(quiz_id, "What is the capital of Norway?", 1,
+		[]answerAlt{
+			{"Oslo", true},
+			{"Bergen", false},
+			{"Trondheim", false},
+			{"Stavanger", false},
+		})
+	createQuestion(quiz_id, "What is the capital of Sweden?", 2,
+		[]answerAlt{
+			{"Stockholm", true},
+			{"Gothenburg", false},
+			{"Malmö", false},
+			{"Uppsala", false},
+		})
 
 }
 
-func runQuery(query string) {
-	_, e := database.DB.Query(query)
-	if e != nil {
-		log.Println(e)
+type answerAlt struct {
+	answer  string
+	correct bool
+}
+
+func createQuestion(quiz_id uuid.UUID, question string, arrangement int, answers []answerAlt) {
+	question_id := uuid.New()
+
+	ctx := context.Background()
+	tx, err := database.DB.BeginTx(ctx, nil)
+	if err != nil {
+		log.Println(err)
 	}
+	tx.Exec(
+		`INSERT INTO questions (id, question, arrangement, quiz_id)
+		VALUES ($1, $2, $3, $4);`,
+		question_id, question, arrangement, quiz_id)
+
+	for _, a := range answers {
+		tx.Exec(
+			`INSERT INTO answer_alternatives (question_id, text, correct)
+			VALUES ($1, $2, $3);`,
+			question_id, a.answer, a.correct)
+	}
+	if err := tx.Commit(); err != nil {
+		log.Println(err)
+	}
+
 }
