94c33b4f7a99d18bfde3dd235b3b064ee88676b8
Kacper Lukasz Nowicki <nokacper24@gmail.com>
2024-02-18 18:53:12 +0100

    feat: :sparkles: set question arrangement automatically on insertion

--- db/migrations/000001_init.down.sql
+++ db/migrations/000001_init.down.sql
@@ -4,6 +4,10 @@ DROP TABLE IF EXISTS user_answers;
 
 DROP TABLE IF EXISTS answer_alternatives;
 
+DROP TRIGGER IF EXISTS set_question_arrangement ON questions;
+
+DROP FUNCTION IF EXISTS set_question_arrangement;
+
 DROP TABLE IF EXISTS questions;
 
 DROP TABLE IF EXISTS quizzes;
--- db/migrations/000001_init.up.sql
+++ db/migrations/000001_init.up.sql
@@ -39,6 +39,29 @@ CREATE TABLE IF NOT EXISTS questions (
     CONSTRAINT question_arrangement UNIQUE (arrangement, quiz_id)
 );
 
+CREATE OR REPLACE FUNCTION set_question_arrangement()
+RETURNS TRIGGER AS $$
+DECLARE
+    max_arrangement INTEGER;
+BEGIN
+    SELECT MAX(arrangement) INTO max_arrangement
+    FROM questions
+    WHERE quiz_id = NEW.quiz_id;
+
+    IF max_arrangement IS NULL THEN
+        max_arrangement := 0;
+    END IF;
+    NEW.arrangement := max_arrangement + 1;
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE OR REPLACE TRIGGER set_question_arrangement
+    BEFORE INSERT ON questions
+    FOR EACH ROW
+    EXECUTE FUNCTION set_question_arrangement();
+
+
 CREATE TABLE IF NOT EXISTS answer_alternatives (
     id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     text TEXT NOT NULL,
