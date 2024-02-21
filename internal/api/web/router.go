e8f62c5bfb16a83931fa8ef45a54fad1c1ea7eb6
Kacper Lukasz Nowicki <nokacper24@gmail.com>
2024-02-21 20:15:16 +0100

    feat: :sparkles: applly auth middalware to routes, make a group for quiz related routes

--- internal/api/web/router.go
+++ internal/api/web/router.go
@@ -1,6 +1,7 @@
 package router
 
 import (
+	"github.com/Molnes/Nyhetsjeger/internal/api/middlewares"
 	"github.com/Molnes/Nyhetsjeger/internal/api/web/handlers"
 	"github.com/Molnes/Nyhetsjeger/internal/api/web/handlers/api"
 	"github.com/labstack/echo/v4"
@@ -15,16 +16,29 @@ func SetupRouter(e *echo.Echo) {
 	e.Pre(middleware.RemoveTrailingSlash())
 	e.Use(middleware.Logger())
 
-	handlers.RegisterQuizHandlers(e)
+	// authentication routes
+	auth_group := e.Group("/auth")
+	handlers.RegisterAuthHandlers(auth_group)
+
+	// TODO add homepage with login button if not authenticated
+
+	quizGroup := e.Group("/quiz")
+	quizGroup.Use(middlewares.IsAuthenticated)
+	handlers.RegisterQuizHandlers(quizGroup)
+
 	dashboardGroup := e.Group("/dashboard")
+	dashboardGroup.Use(middlewares.IsAdmin)
+
 	handlers.RegisterDashboardHandlers(dashboardGroup)
 
 	api_group := e.Group("/api/v1")
+	api_group.Use(middlewares.IsAuthenticated)
 
 	quiz_api_group := api_group.Group("/quiz")
 	api.RegisterQuizApiHandlers(quiz_api_group)
 
 	admin_api_group := api_group.Group("/admin")
+	admin_api_group.Use(middlewares.IsAdmin)
 	api.RegisterAdminApiHandlers(admin_api_group)
 
 	e.Static("/static", "assets")
@@ -32,7 +46,4 @@ func SetupRouter(e *echo.Echo) {
 	// websocket for live updates
 	e.GET("/ws", handlers.WebsocketHandler)
 
-	// authentication routes
-	auth_group :=e.Group("/auth")
-	handlers.RegisterAuthHandlers(auth_group)
 }
