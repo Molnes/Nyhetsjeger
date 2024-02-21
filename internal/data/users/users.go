8946b8dc3005af9c62c989ddf2dc8c4df2f84bc0
Kacper Lukasz Nowicki <nokacper24@gmail.com>
2024-02-21 19:04:58 +0100

    feat: :sparkles: add user store functions to users pacakge

--- internal/data/users/users.go
+++ internal/data/users/users.go
@@ -5,6 +5,12 @@ import (
 	"encoding/gob"
 	"time"
 
+	"database/sql"
+	"encoding/gob"
+	"time"
+
+	"github.com/Molnes/Nyhetsjeger/internal/data/users/user_roles"
+
 	"github.com/Molnes/Nyhetsjeger/internal/data/users/user_roles"
 	"github.com/google/uuid"
 )
@@ -22,23 +28,9 @@ type User struct {
 	RefreshtokenCypher []byte
 }
 
-type UserSessionData struct {
-	ID    uuid.UUID
-	SsoID string
-	Email string
-}
-
-func (u *User) IntoSessionData() UserSessionData {
-	return UserSessionData{
-		ID:    u.ID,
-		SsoID: u.SsoID,
-		Email: u.Email,
-	}
-}
-
 func init() {
-	// Register the UserSessionData struct for gob encoding, needed for session storage
-	gob.Register(UserSessionData{})
+	// Register the User struct for gob encoding
+	gob.Register(User{})
 }
 
 func GetUserByID(db *sql.DB, id uuid.UUID) (*User, error) {
@@ -65,17 +57,6 @@ func CreateUser(db *sql.DB, user *User) (*User, error) {
 		RETURNING id, sso_user_id, email, phone, opt_in_ranking, role, access_token, token_expires_at, refresh_token`,
 		user.ID, user.SsoID, user.Email, user.Phone, user.OptInRanking, user.Role.String(), user.AccessTokenCypher, user.Token_expire, user.RefreshtokenCypher)
 	return scanUserFromFullRow(row)
-}
-
-// Returns the role of the user with the ID provided
-func GetUserRole(db *sql.DB, id uuid.UUID) (user_roles.Role, error) {
-	var role string
-	err := db.QueryRow(
-		`SELECT role
-		FROM users
-		WHERE id = $1`,
-		id).Scan(&role)
-	return user_roles.RoleFromString(role), err
 }
 
 // Updates the user with the ID of the user provided
