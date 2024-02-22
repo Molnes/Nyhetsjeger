6a122e094db6a6c0f6c0bdb1f51b4b84b7141ac9
Ine Sofie <zilerayser@gmail.com>
2024-02-22 14:21:43 +0100

    feat: :sparkles: create simple post quiz function

--- internal/api/web/handlers/api/admin_api.go
+++ internal/api/web/handlers/api/admin_api.go
@@ -1,9 +1,17 @@
 package api
 
 import (
+	"github.com/Molnes/Nyhetsjeger/internal/data/quizzes"
 	"github.com/labstack/echo/v4"
 )
 
 func RegisterAdminApiHandlers(e *echo.Group) {
+	e.POST("/create-new", PostQuiz)
+}
 
+func PostQuiz(c echo.Context) error {
+	// TODO: Add quiz to the database
+
+	quiz, _ := quizzes.CreateDefaultQuiz()
+	return c.JSON(200, quiz)
 }
