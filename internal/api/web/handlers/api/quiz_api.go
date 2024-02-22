31c34e2977d5585d4bda6f5dfbd7ebe8b4d22e5f
Kacper Lukasz Nowicki <nokacper24@gmail.com>
2024-02-22 20:33:36 +0100

    fix: :adhesive_bandage: dont export quizapi handlers

--- internal/api/web/handlers/api/quiz_api.go
+++ internal/api/web/handlers/api/quiz_api.go
@@ -20,28 +20,28 @@ func NewQuizApiHandler(sharedData *config.SharedData) *QuizApiHandler {
 }
 
 func (qah *QuizApiHandler) RegisterQuizApiHandlers(e *echo.Group) {
-	e.GET("/question", qah.GetQuestion)
-	e.GET("/article", qah.GetArticle)
-	e.GET("/articles", qah.GetArticles)
-	e.GET("/:quizId/summary", qah.GetQuizSummary)
+	e.GET("/question", qah.getQuestion)
+	e.GET("/article", qah.getArticle)
+	e.GET("/articles", qah.getArticles)
+	e.GET("/:quizId/summary", qah.getQuizSummary)
 }
 
-func (qah *QuizApiHandler) GetQuestion(c echo.Context) error {
+func (qah *QuizApiHandler) getQuestion(c echo.Context) error {
 	question := "Exampl question?"
 	alts := []string{"alt1", "alt2", "alt3", "alt4"}
 	return utils.Render(c, http.StatusOK, quiz_components.Question(question, alts))
 }
 
-func (qah *QuizApiHandler) GetQuizSummary(c echo.Context) error {
+func (qah *QuizApiHandler) getQuizSummary(c echo.Context) error {
 	return utils.Render(c, http.StatusOK, quiz_components.QuizSummary(users.SampleUserQuizSummary))
 }
 
-func (qah *QuizApiHandler) GetArticle(c echo.Context) error {
+func (qah *QuizApiHandler) getArticle(c echo.Context) error {
 	article := articles.SampleArticles[0]
 	return utils.Render(c, http.StatusOK, quiz_components.ArticleCard(article))
 }
 
-func (qah *QuizApiHandler) GetArticles(c echo.Context) error {
+func (qah *QuizApiHandler) getArticles(c echo.Context) error {
 	articles := articles.SampleArticles
 	return utils.Render(c, http.StatusOK, quiz_components.ArticleList(articles))
 }
