e93dad247375c278e674fd7fd5720dd4b3206bee
Ine Sofie <zilerayser@gmail.com>
2024-02-22 13:36:58 +0100

    feat: :sparkles: function to create default quiz

--- internal/data/quizzes/quizzes.go
+++ internal/data/quizzes/quizzes.go
@@ -1,18 +1,38 @@
 package quizzes
 
 import (
+	"time"
+
 	"github.com/Molnes/Nyhetsjeger/internal/data/questions"
 	"github.com/google/uuid"
 )
 
 type Quiz struct {
-	ID        uuid.UUID
-	Title     string
-	Questions []questions.Question
+	ID             uuid.UUID
+	Title          string
+	AvailableFrom  time.Time
+	AvailableTo    time.Time
+	CreatedAt      time.Time
+	LastModifiedAt time.Time
+	Questions      []questions.Question
 }
 
 func GetQuiz(quizID uuid.UUID) (Quiz, error) {
 	return SampleQuiz, nil
+}
+
+// Create a default quiz
+// This function is used to create a new quiz with default values
+func CreateDefaultQuiz() (Quiz, error) {
+	return Quiz{
+		ID:             uuid.New(),
+		Title:          "Daglig Quiz: " + time.Now().Format("1970-01-01"),
+		AvailableFrom:  time.Now(),
+		AvailableTo:    time.Now().Add(24 * time.Hour),
+		CreatedAt:      time.Now(),
+		LastModifiedAt: time.Now(),
+		Questions:      []questions.Question{},
+	}, nil
 }
 
 var SampleQuiz Quiz = Quiz{
