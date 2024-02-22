e86aa1ca049547b4b2055c8181da59515ba14173
Ine Sofie <zilerayser@gmail.com>
2024-02-22 15:16:42 +0100

    feat: :sparkles: make new quiz redirect to edit page

--- internal/api/web/handlers/api/admin_api.go
+++ internal/api/web/handlers/api/admin_api.go
@@ -1,17 +1,21 @@
 package api
 
 import (
+	"net/http"
+
 	"github.com/Molnes/Nyhetsjeger/internal/data/quizzes"
 	"github.com/labstack/echo/v4"
 )
 
 func RegisterAdminApiHandlers(e *echo.Group) {
-	e.POST("/create-new", PostQuiz)
+	e.POST("/quiz/create-new", PostQuiz)
 }
 
 func PostQuiz(c echo.Context) error {
 	// TODO: Add quiz to the database
 
 	quiz, _ := quizzes.CreateDefaultQuiz()
-	return c.JSON(200, quiz)
+
+	c.Response().Header().Set("HX-Redirect", "/dashboard/quiz/edit/"+quiz.ID.String())
+	return c.Redirect(http.StatusOK, "/dashboard/quiz/edit/"+quiz.ID.String())
 }
--- internal/api/web/handlers/dashboard.go
+++ internal/api/web/handlers/dashboard.go
@@ -4,6 +4,7 @@ import (
 	"net/http"
 
 	"github.com/Molnes/Nyhetsjeger/internal/api/web/views/pages/dashboard_pages"
+	"github.com/Molnes/Nyhetsjeger/internal/data/quizzes"
 	"github.com/Molnes/Nyhetsjeger/internal/utils"
 	"github.com/labstack/echo/v4"
 )
@@ -11,7 +12,7 @@ import (
 // Registers handlers for dashboard related pages
 func RegisterDashboardHandlers(e *echo.Group) {
 	e.GET("", dashboardHomePage)
-	e.GET("/create-quiz", dashboardCreateQuiz)
+	e.GET("/quiz/edit/:quizId", dashboardEditQuiz)
 }
 
 // Renders the dashboard home page
@@ -20,6 +21,7 @@ func dashboardHomePage(c echo.Context) error {
 }
 
 // Renders the page for creating a new quiz
-func dashboardCreateQuiz(c echo.Context) error {
-	return utils.Render(c, http.StatusOK, dashboard_pages.EditQuiz())
+func dashboardEditQuiz(c echo.Context) error {
+	// TODO: Fetch the quiz from the database
+	return utils.Render(c, http.StatusOK, dashboard_pages.EditQuiz(quizzes.Quiz{}))
 }
