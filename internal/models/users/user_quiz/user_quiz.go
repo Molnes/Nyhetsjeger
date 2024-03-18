3c28ad10a29ec3e4f7ae3162a3d5d5c579a212ad
Kacper Lukasz Nowicki <nokacper24@gmail.com>
2024-03-18 21:09:34 +0100

    feat: :sparkles: reimplement function to answer a question

--- internal/models/users/user_quiz/user_quiz.go
+++ internal/models/users/user_quiz/user_quiz.go
@@ -3,6 +3,7 @@ package user_quiz
 import (
 	"database/sql"
 	"errors"
+	"math"
 	"time"
 
 	"github.com/Molnes/Nyhetsjeger/internal/models/questions"
@@ -61,4 +62,75 @@ func StartNextQuestion(db *sql.DB, userID uuid.UUID, quizID uuid.UUID) (*questio
 		return nil, err
 	}
 	return question, nil
+}
+
+
+type UserAnsweredQuestion struct {
+	Question       questions.Question
+	ChosenAnswerID uuid.UUID
+	PointsAwarded  uint
+}
+
+var ErrQuestionAlreadyAnswered = errors.New("user quiz: question already answered")
+
+func AnswerQuestion(db *sql.DB, userId uuid.UUID, questionId uuid.UUID, chosenAlternative uuid.UUID) (*UserAnsweredQuestion, error) {
+	var questionPresentedAt time.Time
+	var chosenAnsweIdNull uuid.UUID
+	var maxPoints uint
+	var timeLimit time.Time
+	err := db.QueryRow(
+		`SELECT question_presented_at, questions.points, questions.time_limit_seconds, chosen_answer_alternative_id
+		FROM user_answers JOIN questions ON user_answers.question_id = questions.id
+		WHERE user_id = $1 AND question_id = $2;`, userId, questionId,
+	).Scan(&questionPresentedAt, &maxPoints, &timeLimit, &chosenAnsweIdNull)
+	if err != nil {
+		return nil, err
+	}
+	if chosenAnsweIdNull != uuid.Nil {
+		return nil, ErrQuestionAlreadyAnswered
+	}
+
+	nowTime := time.Now()
+	pointsAwarded := calculatePoints(questionPresentedAt, nowTime, uint(timeLimit.Second()), maxPoints)
+	_, err = db.Exec(
+		`UPDATE user_answers
+		SET chosen_answer_alternative_id = $1, answered_at = $2, points_awarded = $3
+		WHERE user_id = $4 AND question_id = $5;`,
+		chosenAlternative, nowTime, pointsAwarded, userId, questionId)
+
+	if err != nil {
+		return nil, err
+	}
+
+	question, err := questions.GetQuestionByID(db, questionId)
+	if err != nil {
+		return nil, err
+	}
+
+	return &UserAnsweredQuestion{
+		Question:       *question,
+		ChosenAnswerID: chosenAlternative,
+		PointsAwarded:  pointsAwarded,
+	}, nil
+}
+
+func calculatePoints(questionPresentadAt time.Time, answeredAt time.Time, timeLimit uint, maxPoints uint) uint {
+
+	diff := answeredAt.Sub(questionPresentadAt)
+	secondsTaken := diff.Seconds()
+
+	var pointsAwarded float64
+
+	timeLimitFloat := float64(timeLimit)
+	if secondsTaken < 0.75*timeLimitFloat {
+		pointsAwarded = float64(maxPoints)
+	} else if secondsTaken < 0.5*timeLimitFloat {
+		pointsAwarded = float64(maxPoints) / 2
+	} else {
+		pointsAwarded = float64(maxPoints) / 4
+	}
+
+	rounded := math.RoundToEven(pointsAwarded)
+	return uint(rounded)
+
 }
