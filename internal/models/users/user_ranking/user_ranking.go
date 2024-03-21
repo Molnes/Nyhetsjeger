a3fd0a1e5e8c87e737b4b990c0e21ea128832b8b
Molnes <jakob.holkestad.molnes@gmail.com>
2024-03-21 13:35:43 +0100

    fix: get the concatinated string from the db

--- internal/models/users/user_ranking/user_ranking.go
+++ internal/models/users/user_ranking/user_ranking.go
@@ -3,15 +3,14 @@ package user_ranking
 import "database/sql"
 
 type UserRanking struct {
-	Username_adjective string
-    Username_noun string
+	Username string
 	Points int
 }
 
 // Returns the ranking of all users who have opted in to the ranking.
 func GetRanking(db *sql.DB) ([]UserRanking, error) {
 	rows, err := db.Query(`
-        SELECT username_adjective, username_noun, pt.sum_points
+        SELECT CONCAT(username_adjective, ' ', username_noun) AS Username, pt.sum_points
 FROM users
 JOIN (
 SELECT user_id, SUM(points_awarded) AS sum_points FROM user_answers
@@ -30,7 +29,7 @@ ORDER BY pt.sum_points DESC;
 	var rankings []UserRanking
 	for rows.Next() {
 		var ranking UserRanking
-		if err := rows.Scan(&ranking.Username_adjective, &ranking.Username_noun, &ranking.Points); err != nil {
+		if err := rows.Scan(&ranking.Username, &ranking.Points); err != nil {
                         return nil, err
                 }
                 rankings = append(rankings, ranking)
