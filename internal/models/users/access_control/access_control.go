d663062f002a65c466e8cf8616eb9b7019cab8bc
Kacper Lukasz Nowicki <nokacper24@gmail.com>
2024-03-25 19:02:14 +0100

    feat: :sparkles: implement functions for role setting and access control

--- internal/models/users/access_control/access_control.go
+++ internal/models/users/access_control/access_control.go
@@ -2,6 +2,7 @@ package access_control
 
 import (
 	"database/sql"
+	"errors"
 
 	"github.com/Molnes/Nyhetsjeger/internal/models/users/user_roles"
 )
@@ -42,4 +43,76 @@ func GetAllAdmins(db *sql.DB) (*[]UserAdmin, error) {
 	}
 
 	return &admins, nil
+}
+
+// Adds a QuizAdmin role to a user with the given email.
+//
+// If the user does not exist, the role is preassigned and will be assigned upon registration.
+func AddAdmin(db *sql.DB, email string) (*UserAdmin, error) {
+	var isActive bool
+	err := assignRoleToUseByEmail(db, email, user_roles.QuizAdmin)
+	if err == nil {
+		isActive = true
+	} else {
+		if err != errNoUserFound {
+			return nil, err
+		} else {
+			err = preAssignRoleToUserByEmail(db, email, user_roles.QuizAdmin)
+			if err != nil {
+				return nil, err
+			}
+			isActive = false
+		}
+	}
+
+	return &UserAdmin{
+		Email:    email,
+		IsActive: isActive,
+		Role:     user_roles.QuizAdmin,
+	}, nil
+}
+
+var errNoUserFound = errors.New("no user with given email found")
+
+// Assigns a role to a user with the given email.
+func assignRoleToUseByEmail(db *sql.DB, email string, role user_roles.Role) error {
+	result, err := db.Exec(`
+	UPDATE users
+	SET role=$1
+	WHERE email=$2;`, role.String(), email)
+	if err != nil {
+		return err
+	}
+	rowsAffected, err := result.RowsAffected()
+	if err != nil {
+		return err
+	}
+	if rowsAffected < 1 {
+		return errNoUserFound
+	}
+	return nil
+}
+
+// Preassigns a role to a user with the given email.
+func preAssignRoleToUserByEmail(db *sql.DB, email string, role user_roles.Role) error {
+	_, err := db.Exec(`
+	INSERT INTO preassigned_roles(email, role)
+	VALUES ($1, $2);`, email, role.String())
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+// Revokes the QuizAdmin role from a user with the given email.
+func RevokeAdmin(db *sql.DB, email string) error {
+	return assignRoleToUseByEmail(db, email, user_roles.User)
+}
+
+// Removes a preassigned QuizAdmin role from the given email.
+func RemovePreassignedAdmin(db *sql.DB, email string) error {
+	_, err := db.Exec(`
+	DELETE FROM preassigned_roles
+	WHERE email=$1;`, email)
+	return err
 }
