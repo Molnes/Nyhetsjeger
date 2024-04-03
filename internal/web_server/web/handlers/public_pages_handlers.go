d3e278f83741286c74b544db2689072e0c5aab41
Kacper Lukasz Nowicki <nokacper24@gmail.com>
2024-04-03 13:25:56 +0200

    feat: :sparkles: redirect signed in user from / to /quiz or /dashboard depending on their role

--- internal/web_server/web/handlers/public_pages_handlers.go
+++ internal/web_server/web/handlers/public_pages_handlers.go
@@ -1,10 +1,13 @@
 package handlers
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/Molnes/Nyhetsjeger/internal/config"
 	"github.com/Molnes/Nyhetsjeger/internal/models/sessions"
+	"github.com/Molnes/Nyhetsjeger/internal/models/users"
+	"github.com/Molnes/Nyhetsjeger/internal/models/users/user_roles"
 	"github.com/Molnes/Nyhetsjeger/internal/utils"
 	"github.com/Molnes/Nyhetsjeger/internal/web_server/web/views/pages/public_pages"
 	"github.com/labstack/echo/v4"
@@ -26,6 +29,26 @@ func (pph *PublicPagesHandler) RegisterPublicPages(e *echo.Echo) {
 }
 
 func (pph *PublicPagesHandler) homePage(c echo.Context) error {
+	session, err := pph.sharedData.SessionStore.Get(c.Request(), sessions.SESSION_NAME)
+	if err == nil {
+		userData := session.Values[sessions.USER_DATA_VALUE]
+		if userData != nil {
+			sessiondata, ok := userData.(users.UserSessionData)
+			if !ok {
+				return fmt.Errorf("AuthenticationOnHomePage: failed to cast user data to UserSessionData")
+			}
+			role, err := users.GetUserRole(pph.sharedData.DB, sessiondata.ID)
+			if err != nil {
+				return err
+			}
+			switch role {
+			case user_roles.OrganizationAdmin, user_roles.QuizAdmin:
+				return c.Redirect(http.StatusTemporaryRedirect, "/dashboard")
+			default:
+				return c.Redirect(http.StatusTemporaryRedirect, "/quiz")
+			}
+		}
+	}
 	return utils.Render(c, http.StatusOK, public_pages.HomePage())
 }
 
