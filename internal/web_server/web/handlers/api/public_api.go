105ea1753dbe0657e6e488ec31b3703fe0e49169
Kacper Lukasz Nowicki <nokacper24@gmail.com>
2024-04-15 18:11:46 +0200

    feat: :sparkles: start implementing post guest answer handler

--- internal/web_server/web/handlers/api/public_api.go
+++ internal/web_server/web/handlers/api/public_api.go
@@ -1,7 +1,13 @@
 package api
 
 import (
+	"net/http"
+
 	"github.com/Molnes/Nyhetsjeger/internal/config"
+	"github.com/Molnes/Nyhetsjeger/internal/models/users/user_quiz"
+	utils "github.com/Molnes/Nyhetsjeger/internal/utils"
+	"github.com/Molnes/Nyhetsjeger/internal/web_server/web/views/components/quiz_components/play_quiz_components"
+	"github.com/google/uuid"
 	"github.com/labstack/echo/v4"
 )
 
@@ -16,15 +22,29 @@ func NewPublicApiHandler(sharedData *config.SharedData) *publicApiHandler {
 
 // Registers the public api handlers to the given echo group
 func (h *publicApiHandler) RegisterPublicApiHandlers(g *echo.Group) {
-	g.POST("answer", h.postAnswer)
+	g.POST("/user-answer", h.postAnswer)
 }
 
 func (h *publicApiHandler) postAnswer(c echo.Context) error {
-	// questionId, err := uuid.Parse(c.QueryParam("question-id"))
-	// if err != nil {
-	// 	return echo.NewHTTPError(http.StatusBadRequest, "Manglende eller ugyldig question-id")
-	// }
+	questionID, err := uuid.Parse(c.QueryParam("question-id"))
+	if err != nil {
+		return echo.NewHTTPError(http.StatusBadRequest, "Invalid or missing question-id")
+	}
+	pickedAnswerID, err := uuid.Parse(c.FormValue("answer-id"))
+	if err != nil {
+		return echo.NewHTTPError(http.StatusBadRequest, "Invalid or missing answer-id in formdata")
+	}
 
-	// return utils.Render(play_quiz_components.FeedbackButtons())
-	return nil
+	// TODO 
+	answered, err := user_quiz.AnswerQuestion(h.sharedData.DB, uuid.UUID{}, questionID, pickedAnswerID)
+	if err != nil {
+		if err == user_quiz.ErrQuestionAlreadyAnswered {
+			return echo.NewHTTPError(http.StatusConflict, "Question already answered")
+		} else {
+			return err
+		}
+	}
+
+	return utils.Render(c, http.StatusOK, play_quiz_components.FeedbackButtons(answered, true))
+
 }
