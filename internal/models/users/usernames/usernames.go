04cc2d2634f14ffb33a46e7b8b0ba902f0d0c499
Oransj <98455426+Oransj@users.noreply.github.com>
2024-04-16 12:55:51 +0200

    refactor: :recycle: refactor to use two queries

--- internal/models/users/usernames/usernames.go
+++ internal/models/users/usernames/usernames.go
@@ -2,17 +2,78 @@ package usernames
 
 import (
 	"database/sql"
-	"math"
+	"log"
+
+	"github.com/lib/pq"
 )
 
 type UsernameAdminInfo struct {
-	Adjectives []string
-	Nouns      []string
-	AdjPage      int
-	NounPage      int
-	AdjWordCount int
-	NounWordCount int
+	Adjectives       []string
+	Nouns            []string
+	AdjPage          int
+	NounPage         int
+	AdjWordCount     int
+	NounWordCount    int
 	UsernamesPerPage int
+}
+
+func (uai *UsernameAdminInfo) setUaiAdjInfo(db *sql.DB) error {
+
+	log.Println("----------------------------------->>>")
+	log.Println(uai.AdjPage)
+
+	err := db.QueryRow(`
+	WITH offsetvalue AS (
+		SELECT (
+				CASE 
+					WHEN $2 < 1 THEN 1
+					WHEN $2 > (SELECT COUNT(*) FROM adjectives) / $1
+						THEN CEIL((SELECT COUNT(*) FROM adjectives)::float8 / $1::float8)
+					ELSE $2
+				END
+			) as value
+		)
+		
+		SELECT ARRAY (
+		SELECT adjective
+		FROM adjectives
+		ORDER BY adjective ASC
+		LIMIT $1 OFFSET $1 * (offsetvalue.value - 1)
+		) as foo, COUNT(a.*), offsetvalue.value
+		FROM adjectives a, offsetvalue
+		GROUP BY offsetvalue.value;`,
+	uai.UsernamesPerPage, uai.AdjPage).Scan(pq.Array(&uai.Adjectives), &uai.AdjWordCount,  &uai.AdjPage)
+
+	log.Println(uai.AdjPage)
+	log.Println("<<<-----------------------------------")
+
+	return err
+}
+
+func (uai *UsernameAdminInfo) setUaiNounInfo(db *sql.DB) error {
+	err := db.QueryRow(
+		`WITH offsetvalue AS (
+			SELECT (
+					CASE 
+						WHEN $2 < 1 THEN 1
+						WHEN $2 > (SELECT COUNT(*) FROM nouns) / $1
+							THEN CEIL((SELECT COUNT(*) FROM nouns)::float8 / $1::float8)
+						ELSE $2
+					END
+				) as value
+			)
+			
+			SELECT ARRAY (
+			SELECT noun
+			FROM nouns
+			ORDER BY noun ASC
+			LIMIT $1 OFFSET $1 * (offsetvalue.value - 1)
+			) as foo, COUNT(a.*), offsetvalue.value
+			FROM nouns a, offsetvalue
+			GROUP BY offsetvalue.value;`,
+		uai.UsernamesPerPage, uai.NounPage).Scan(pq.Array(&uai.Nouns), &uai.NounWordCount,  &uai.NounPage)
+
+	return err
 }
 
 // GetUsernameAdminInfo returns a UsernameAdminInfo struct containing the adjectives and nouns and relevant information
@@ -20,72 +81,20 @@ type UsernameAdminInfo struct {
 func GetUsernameAdminInfo(db *sql.DB, adjPage int, nounPage int, usernamesPerPage int) (*UsernameAdminInfo, error) {
 	uai := UsernameAdminInfo{}
 
-	//Get the total number of words in the adjectives and nouns tables
-	err := db.QueryRow(`SELECT COUNT(*) FROM adjectives;`).Scan(&uai.AdjWordCount)
-	if err != nil {
-		return nil, err
-	}
-	err = db.QueryRow(`SELECT COUNT(*) FROM nouns;`).Scan(&uai.NounWordCount)
-	if err != nil {
-		return nil, err
-	}
-
-	//Insert adjective page and noun page into the struct and make sure they are within bounds
-	if adjPage < 1 {
-		adjPage = 1
-	}
-	if nounPage < 1 {
-		nounPage = 1
-	}
-	if adjPage > (uai.AdjWordCount / usernamesPerPage) {
-		adjPage = int(math.Ceil((float64(uai.AdjWordCount) / float64(usernamesPerPage))))
-	}
-	if nounPage > (uai.NounWordCount / usernamesPerPage) {
-		nounPage = int(math.Ceil((float64(uai.NounWordCount) / float64(usernamesPerPage))))
-	}
 	uai.AdjPage = adjPage
 	uai.NounPage = nounPage
 	uai.UsernamesPerPage = usernamesPerPage
 
-	rows, err := db.Query(`
-					SELECT adjective 
-						FROM adjectives
-						ORDER BY adjective ASC
-							LIMIT $1 OFFSET $2;`,
-		usernamesPerPage, usernamesPerPage*(adjPage-1))
-	if err != nil {
-		return nil, err
-	}
-	defer rows.Close()
+	err := uai.setUaiAdjInfo(db)
 
-	//Loop through the adjectives
-	for rows.Next() {
-		var adjective string
-		err = rows.Scan(&adjective)
-		if err != nil {
-			return nil, err
-		}
-		uai.Adjectives = append(uai.Adjectives, adjective)
-	}
-
-	rows, err = db.Query(`
-	SELECT noun
-		FROM nouns
-		ORDER BY noun ASC
-			LIMIT $1 OFFSET $2;`,
-		usernamesPerPage, usernamesPerPage*(nounPage-1))
 	if err != nil {
 		return nil, err
 	}
 
-	//Loop through the nouns
-	for rows.Next() {
-		var noun string
-		err = rows.Scan(&noun)
-		if err != nil {
-			return nil, err
-		}
-		uai.Nouns = append(uai.Nouns, noun)
+	err = uai.setUaiNounInfo(db)
+
+	if err != nil {
+		return nil, err
 	}
 
 	return &uai, nil
