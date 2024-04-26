0729230b9d214d1d38e560da850800d6d6872701
Oransj <98455426+Oransj@users.noreply.github.com>
2024-04-26 12:39:23 +0200

    refactor: :recycle: add return errortext when error

--- internal/web_server/web/handlers/api/admin_api.go
+++ internal/web_server/web/handlers/api/admin_api.go
@@ -1018,7 +1018,7 @@ func (aah *AdminApiHandler) getUsernamePages(c echo.Context) error {
 
 	uai, err := usernames.GetUsernameAdminInfo(aah.sharedData.DB, adjPage, nounPage, pages, search)
 	if err != nil {
-		return err
+		return utils.Render(c, http.StatusBadRequest, components.ErrorText("error-username", "Noe gikk galt under henting av data. Prøv igjen senere. Hvis problemet vedvarer, kontakt administrator."))
 	}
 
 	// Creates a relative path with the queryparams to update the url of
