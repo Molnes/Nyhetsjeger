15fd7ba22e0bf9b1d75a510a95fd2afd640f34fe
Molnes <jakob.holkestad.molnes@gmail.com>
2024-04-27 21:47:30 +0200

    test: adding some tests

---
+++ internal/utils/date/date_utils_test.go
@@ -0,0 +1,25 @@
+package date_utils_test
+
+import (
+	"github.com/Molnes/Nyhetsjeger/internal/utils/date"
+	"testing"
+	"time"
+)
+
+func TestDateToNorwegianString(t *testing.T) {
+	date := time.Date(2021, time.January, 1, 0, 0, 0, 0, time.UTC)
+    expected := "Fredag, 1. januar 00:00"
+	actual := date_utils.DateToNorwegianString(date)
+	if actual != expected {
+		t.Errorf("Expected %s, but got %s", expected, actual)
+	}
+}
+
+func TestDateToNorwegianString2(t *testing.T) {
+        date := time.Date(2021, time.January, 2, 0, 0, 0, 0, time.UTC)
+        expected := "Lørdag, 2. januar 00:00"
+        actual := date_utils.DateToNorwegianString(date)
+        if actual != expected {
+                t.Errorf("Expected %s, but got %s", expected, actual)
+        }
+}
