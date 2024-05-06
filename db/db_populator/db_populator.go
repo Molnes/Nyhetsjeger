ac91a0bd3f0ec1bc54591a0245e475060c3f3722
Kacper Lukasz Nowicki <nokacper24@gmail.com>
2024-05-06 15:01:52 +0200

    test: :white_check_mark: add dummy user test data

--- db/db_populator/db_populator.go
+++ db/db_populator/db_populator.go
@@ -9,12 +9,14 @@ import (
 	"time"
 
 	"github.com/Molnes/Nyhetsjeger/internal/models/articles"
+	"github.com/Molnes/Nyhetsjeger/internal/models/users"
 	"github.com/google/uuid"
 )
 
 func PopulateDbWithTestData(db *sql.DB) {
 	createSampleQuiz(db, "Ukentlig quiz 1")
 	createSampleQuiz(db, "Ukentlig quiz 2")
+	createTestUser(db)
 }
 
 func createSampleQuizArticle(db *sql.DB, quizID uuid.UUID) {
@@ -162,4 +164,24 @@ func getSampleAricle() *articles.Article {
 	}
 
 	return &article
+}
+
+func createTestUser(db *sql.DB) error {
+	_, err := db.Exec(`INSERT INTO adjectives VALUES ('adj1'), ('adj2');`)
+	if err != nil {
+		return err
+	}
+	_, err = db.Exec(`INSERT INTO nouns VALUES ('noun1'), ('noun2');`)
+	if err != nil {
+		return err
+	}
+	ctx := context.Background()
+	_, err = users.CreateUser(db, ctx, &users.PartialUser{
+		SsoID:        "test_user_sso_id",
+		Email:        "test_user@email.com",
+		AccessToken:  "",
+		RefreshToken: "",
+		TokenExpire:  time.Now().Add(time.Hour),
+	})
+	return err
 }
